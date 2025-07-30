9207ef9a3c138d2e9c418b257bcc91086e543f8a
ukri41 <ukri41@gmail.com>
2025-07-30 20:59:44 +0200

    Playing with script that rotates the sprite

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+   
+    void Start()
+    {
+        
+    }
+
+   
+    void Update()
+    {
+        transform.Rotate(0,0,0.1f);
+    }
+}
